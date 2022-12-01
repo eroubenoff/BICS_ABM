ccd0015d77d247a06d689d30c70187a48563fe4b
Ethan Roubenoff <eroubenoff@berkeley.edu>
2022-12-01 10:34:46 -0800

    removed v1

--- unused/V1/configuration_mixing.py
+++
@@ -1,4 +0,0 @@
-from load_data import sim_pop
-
-pop = sim_pop(1000)
-
--- unused/V1/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-# python setup.py build_ext --inplace
-setup(ext_modules = cythonize('Population_cy.pyx'),
-      include_dirs=[numpy.get_include()])
